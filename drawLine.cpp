fc6ab365c6c85a55e6d946a052ca1ba7967927a0
mboerwinkle <mboerwinkle@gmail.com>
2017-03-08 01:25:22 +0000

    threading. and octree initial implementation.

--- drawLine.cpp
+++ drawLine.cpp
@@ -3,8 +3,9 @@
 #include <math.h>
 //some weird bastard of a line drawing algo. Almost bresenham but with z spliced in in a strange and terrible way.
 extern int MAXX, MAXY, MAXZ;
+extern char* volume;
 extern int idx(int x, int y, int z);
-void drawPt(char* volume, int x, int y, int z){
+void drawPt(int x, int y, int z){
 	x+=MAXX/2;
 	y+=MAXY/2;
 	z+=MAXZ/2;//so that build volume centers on origin.
@@ -12,46 +13,46 @@ void drawPt(char* volume, int x, int y, int z){
 		volume[idx(x, y, z)] = '_';
 	}
 }
-void drawLine(char* volume, int x1, int y1, int z1, int x2, int y2, int z2){
+void drawLine(int x1, int y1, int z1, int x2, int y2, int z2){
 	int rotated = 0;
 	if(abs(x2-x1) < abs(y2-y1)){//rotate plane
 		int temp = x1;
 		x1 = -y1;
 		y1 = temp;
-                temp = x2;
-                x2 = -y2;
-                y2 = temp;
+		temp = x2;
+		x2 = -y2;
+		y2 = temp;
 		rotated = 1;
 	}
-        if(x1 > x2){//line is going backwards; switch points
-        	int temp = x1;
-        	x1 = x2;
-        	x2 = temp;
-        	temp = y1;
-        	y1 = y2;
-        	y2 = temp;
+	if(x1 > x2){//line is going backwards; switch points
+		int temp = x1;
+		x1 = x2;
+		x2 = temp;
+		temp = y1;
+		y1 = y2;
+		y2 = temp;
 		temp = z1;
 		z1 = z2;
 		z2 = temp;
-        }
-        double slope = (double)(y2-y1)/(double)(x2-x1);
-        int sign = 1;
-        if(slope < 0){
-        	sign = -1;
-        }
-        double err = 0;
+	}
+	double slope = (double)(y2-y1)/(double)(x2-x1);
+	int sign = 1;
+	if(slope < 0){
+		sign = -1;
+	}
+	double err = 0;
 	int fullXdist = x2-x1;
-        for(;x1 <= x2; x1++){
+   	for(;x1 <= x2; x1++){
 		double completion = 1.0-fabs((double)(x2-x1)/(double)(fullXdist));
 		if(rotated){
-                	drawPt(volume, y1, -x1, z1+completion*(z2-z1));
+			drawPt(y1, -x1, z1+completion*(z2-z1));
 		}else{
-                	drawPt(volume, x1, y1, z1+completion*(z2-z1));
+			drawPt(x1, y1, z1+completion*(z2-z1));
 		}
-                err+=slope;
-                if(fabs(err)>=1){
-                	y1+=sign;
-                	err-=sign;
-                }
+		err+=slope;
+		if(fabs(err)>=1){
+			y1+=sign;
+			err-=sign;
+		}
 	}
 }
--- main.cpp
+++ main.cpp
@@ -2,25 +2,28 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include "camera.h"
+#include "voxtree.h"
 #include "delay.h"
 using namespace cv;
 #define FRAMERATE 60
 
 double FOV = 0.976;
 double VERT_FOV = 0.523;
-int distToCenter = 550;
-double interval = M_PI/8;
-int MAXX = 160, MAXY = 160, MAXZ = 160;//octree
+int distToCenter = 550*1.25;
+int frames = 8;
+Voxtree volume(160*1.25);
+int rayDist;//absolute longest a ray has to travel.
 Camera cam(1);
 
 void* frameCapture(void *null);
 void norm(double* vec);
-extern void drawLine(char* volume, int x1, int y1, int z1, int x2, int y2, int z2);
-extern void stitchVoxels(char* volume);
-extern void hollow(char* volume);
-extern void makeStl(char* volume);
+
+extern void makeStl();
+void* frameProcess(void *null);
 
 int main(){
+	rayDist = sqrt(3*volume.size*volume.size)*0.5+distToCenter;
+
 	startWindowThread();
 	pthread_t frameCapThread;
 	pthread_create(&frameCapThread, NULL, frameCapture, NULL);
@@ -47,58 +50,16 @@ int main(){
 }
 
 void* frameCapture(void *null){
-	char *view = (char*)calloc(cam.width*cam.height, sizeof(char));
-	char *volume = (char*)calloc(MAXX*MAXY*MAXZ, sizeof(char));
+	volume = (char*)calloc(MAXX*MAXY*MAXZ, sizeof(char));
 	memset(volume, '+', MAXX*MAXY*MAXZ);
-	for(double angle = 0.0; angle < 2*M_PI; angle+=interval){
-		//Defining camera location and direction camera is facing in rotated space. We keep the model space still (it doesnt rotate).
-		int camPosX = cos(angle)*distToCenter;
-		int camPosY = sin(angle)*distToCenter;
-		int camPosZ = 0;
-		double camDir[3] = {-(double)camPosX, -(double)camPosY, 0.0};
-		norm(camDir);
-////
-
+	pthread_t frameProcThread[frames];
+	for(int frameIdx = 0; frameIdx < frames; frameIdx++){
+		double angle = frameIdx*2*M_PI/frames;
 		printf("Press enter when oriented to %.2lf degrees\n", angle*180/M_PI);
 		getchar();
-		while(cam.dataThreadLock){}
-		cam.dataThreadLock = 1;
-		for(int x = 0; x < cam.width; x++){
-			for(int y = 0; y < cam.height; y++){
-				view[x+y*cam.width] = (cam.getBrightness(x, y) < cam.darkThreshold)? 0 : 1;//if below threshold then save 0, else 1
-			}
-		}
-		cam.dataThreadLock = 0;
-		puts("Processing Frame...");
-		int rayDist = sqrt(MAXX*MAXX+MAXY*MAXY+MAXZ*MAXZ)*0.5+distToCenter;//absolute longest a ray has to travel.
-		double xsteps = sin(0.5*(FOV/cam.width))*2*rayDist;
-		double ysteps = sin(0.5*(VERT_FOV/cam.height))*2*rayDist;
-		printf("xsteps %lf ysteps %lf\n", xsteps, ysteps);
-		for(int x = 0; x < cam.width; x++){
-			for(int y = 0; y < cam.height; y++){
-				char val = view[x+y*cam.width];
-				if(val){//cut out this vector//FIXME line drawing robust to big deltaZ//FIXME dynamic stl file size.
-					for(double dlocx = -0.5; dlocx < 0.5; dlocx+=1/xsteps){
-					for(double dlocy = -0.5; dlocy < 0.5; dlocy+=1/ysteps){
-						double loc[2] = {((double)x+dlocx)/cam.width-0.5, ((double)y+dlocy)/cam.height-0.5};//location of the point on the view screen.
-						double dirAngle = loc[0]*FOV;//angle from camera forwards
-						double dirPitch = loc[1]*VERT_FOV;//angle upwards from camera forwards
-						double finalAngle = angle+M_PI+dirAngle;//absolute angle
-						double vec[3] = {cos(finalAngle)*cos(dirPitch),sin(finalAngle)*cos(dirPitch),sin(dirPitch)};//absolute point direction vector
-						drawLine(volume, camPosX, camPosY, camPosZ, camPosX+vec[0]*rayDist, camPosY+vec[1]*rayDist, camPosZ+vec[2]*rayDist);
-					}}
-				}
-			}
-		}
-		puts("Processing Done.");
+		pthread_create(&(frameProcThread[frameIdx]), NULL, frameProcess, &angle);
 	}
-	free(view);
-	puts("Stitching Voxels...");
-	stitchVoxels(volume);
-	puts("Voxels Stitched.");
-	puts("Hollowing Model.");
-	hollow(volume);
-	puts("Model Hollowed.");
+	while(volumeDataLock){}
 	puts("Writing to file...");
 	FILE* fp = fopen("output.dat", "w");
 	fwrite(volume, sizeof(char), MAXX*MAXY*MAXZ, fp);
